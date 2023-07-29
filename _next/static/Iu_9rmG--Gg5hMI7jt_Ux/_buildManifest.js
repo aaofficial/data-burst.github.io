abe5cfb4b7612c17eee3e179ae3811bcd447fd9c
Niyusha Baghayi <niyusha.baghayi@gmail.com>
2023-07-29 17:29:22 +0330

    :tada: Feat: Add deployment of the website package

---
+++ _next/static/Iu_9rmG--Gg5hMI7jt_Ux/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/285-1712a0eb8c02ab45.js","static/chunks/pages/index-25ecae8f713ce4b3.js"],"/_error":["static/chunks/pages/_error-0260c064403af775.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
